b866695ba6597650efc2d6894b5b6ffe883664c6
Revathi G <revathikrishna0323@gmail.com>
2022-10-11 23:16:51 +0530

    written code in EmployeeImpl.java and reformatted the code

--- src/main/java/com/io/github/EmployeeImpl.java
+++ src/main/java/com/io/github/EmployeeImpl.java
@@ -7,4 +7,22 @@
 package com.io.github;
 
 public class EmployeeImpl {
+    public static void main(String[] args) {
+
+        Manager manager = new Manager("Jane", "e00123", "03/03/1999", 23, 3000, 4, 8);
+
+        // Display Manager details
+        System.out.println("The Employee Name   is : " + manager.getEmployeeName());
+        System.out.println("The Employee Code   is : " + manager.getEmployeeCode());
+        System.out.println("The Employee DOB    is : " + manager.getDateOfBirth());
+        System.out.println("The Employee Age    is : " + manager.getAge());
+        System.out.println("The Employee Salary is : " + manager.getSalary());
+        System.out.println("The Employee Team Size is    : " + manager.getTeamSize());
+        System.out.println("The Employee Team Ratings is : " + manager.getTeamRatings() + " stars");
+        System.out.println("The incremented salary : " + manager.calculateIncrement(1.25f));
+    }
 }
+}
+
+
+
--- src/main/java/com/io/github/Main.java
+++
@@ -1,7 +0,0 @@
-package com.io.github;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
