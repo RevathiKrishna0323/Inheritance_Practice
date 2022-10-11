156bbf06b737ea68f6069308612cdb575a59658e
Revathi G <revathikrishna0323@gmail.com>
2022-10-11 22:59:45 +0530

    created getter and setter methods to the attributes of the  Employee class

--- src/main/java/com/io/github/Employee.java
+++ src/main/java/com/io/github/Employee.java
@@ -24,4 +24,44 @@ public class Employee {
         this.age = age;
         this.salary = salary;
     }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
 }
